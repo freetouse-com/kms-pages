240585d9f704f800c162d40964d59c98b7bbcd46
若非 <rf.wangchn@foxmail.com>
2023-03-24 14:58:43 +0800

    feat: 实现复制文本功能

---
+++ src/components/CopyButton/index.tsx
@@ -0,0 +1,42 @@
+/**
+ * @name 复制按钮
+ * @description 复制文本成功后，会将文本写入到剪切板，同时弹出`复制成功`的消息。
+ */
+
+import React, { useRef } from "react";
+import { Button, message } from "antd";
+
+interface CopyButton {
+  children: JSX.Element;
+  color: string;
+  text: string;
+}
+
+const CopyButton: React.FC<CopyButton> = ({ children, color, text }) => {
+  const inputRef = useRef(null);
+
+  async function handleCopyClick() {
+    await navigator.clipboard.writeText(text);
+    message.success("复制成功");
+  }
+
+  return (
+    <>
+      <input
+        type="text"
+        value={text}
+        ref={inputRef}
+        style={{ display: "none" }}
+      />
+      <Button
+        type="primary"
+        style={{ backgroundColor: "#25c2a0" }}
+        onClick={handleCopyClick}
+      >
+        复制秘钥
+      </Button>
+    </>
+  );
+};
+
+export default CopyButton;
