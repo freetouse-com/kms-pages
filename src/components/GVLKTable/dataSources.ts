9e5b0f41e0d03580db9ead11aad9eb848f261995
smpower <rf.wangchn@foxmail.com>
2023-03-25 23:59:49 +0800

    feat: 编写 GVLK 表格组件

---
+++ src/components/GVLKTable/dataSources.ts
@@ -0,0 +1,468 @@
+export default {
+  WINDOWS_SERVER_2022: {
+    key: "Windows Server 2022",
+    value: [
+      {
+        version: "Windows Server 2022 Datacenter",
+        key: "WX4NM-KYWYW-QJJR4-XV3QB-6VM33",
+      },
+      {
+        version: "Windows Server 2022 Datacenter Azure Edition",
+        key: "NTBV8-9K7Q8-V27C6-M2BTV-KHMXV",
+      },
+      {
+        version: "Windows Server 2022 Standard",
+        key: "VDYBN-27WPP-V4HQT-9VMD4-VMK7H",
+      },
+    ],
+  },
+  WINDOWS_SERVER_2019: {
+    key: "Windows Server 2019",
+    value: [
+      {
+        version: "Windows Server 2019 Datacenter",
+        key: "WMDGN-G9PQG-XVVXX-R3X43-63DFG",
+      },
+      {
+        version: "Windows Server 2019 Standard",
+        key: "N69G4-B89J2-4G8F4-WWYCC-J464C",
+      },
+      {
+        version: "Windows Server 2019 Essentials",
+        key: "WVDHN-86M7X-466P6-VHXV7-YY726",
+      },
+    ],
+  },
+  WINDOWS_SERVER_2016: {
+    key: "Windows Server 2016",
+    value: [
+      {
+        version: "Windows Server 2019 Datacenter",
+        key: "WMDGN-G9PQG-XVVXX-R3X43-63DFG",
+      },
+      {
+        version: "Windows Server 2019 Standard",
+        key: "N69G4-B89J2-4G8F4-WWYCC-J464C",
+      },
+      {
+        version: "Windows Server 2019 Essentials",
+        key: "WVDHN-86M7X-466P6-VHXV7-YY726",
+      },
+      {
+        version: "Windows Server 2016 Datacenter",
+        key: "CB7KF-BWN84-R7R2Y-793K2-8XDDG",
+      },
+      {
+        version: "Windows Server 2016 Standard",
+        key: "WC2BQ-8NRM3-FDDYY-2BFGV-KHKQY",
+      },
+      {
+        version: "Windows Server 2016 Essentials",
+        key: "JCKRF-N37P4-C2D82-9YXRT-4M63B",
+      },
+    ],
+  },
+  WINDOWS_SERVER_20H2_2004_1909_1809: {
+    key: "Windows Server，版本 20H2、2004、1909、1903 和 1809",
+    value: [
+      {
+        version: "Windows Server Datacenter",
+        key: "6NMRW-2C8FM-D24W7-TQWMY-CWH2D",
+      },
+      {
+        version: "Windows Server Standard",
+        key: "N2KJX-J94YW-TQVFB-DG9YT-724CC",
+      },
+    ],
+  },
+  WINDOWS_11_10: {
+    key: "Windows 11 和 Windows 10（半年频道版本）",
+    value: [
+      {
+        version: "Windows 11 专业版",
+        key: "6NMRW-2C8FM-D24W7-TQWMY-CWH2D",
+      },
+      {
+        version: "Windows 10 专业版",
+        key: "W269N-WFGWX-YVC9B-4J6C9-T83GX",
+      },
+      {
+        version: "Windows 11 专业版 N",
+        key: "MH37W-N47XK-V7XM9-C7227-GCQG9",
+      },
+      {
+        version: "Windows 10 专业版 N",
+        key: "MH37W-N47XK-V7XM9-C7227-GCQG9",
+      },
+      {
+        version: "Windows 11 专业工作站版",
+        key: "NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J",
+      },
+      {
+        version: "Windows 10 专业工作站版",
+        key: "NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J",
+      },
+      {
+        version: "Windows 11 专业工作站版 N",
+        key: "9FNHH-K3HBT-3W4TD-6383H-6XYWF",
+      },
+      {
+        version: "Windows 10 专业工作站版 N",
+        key: "9FNHH-K3HBT-3W4TD-6383H-6XYWF",
+      },
+      {
+        version: "Windows 11 专业教育版",
+        key: "6TP4R-GNPTD-KYYHQ-7B7DP-J447Y",
+      },
+      {
+        version: "Windows 10 专业教育版",
+        key: "6TP4R-GNPTD-KYYHQ-7B7DP-J447Y",
+      },
+      {
+        version: "Windows 11 专业教育版 N",
+        key: "YVWGF-BXNMC-HTQYQ-CPQ99-66QFC",
+      },
+      {
+        version: "Windows 10 专业教育版 N",
+        key: "YVWGF-BXNMC-HTQYQ-CPQ99-66QFC",
+      },
+      {
+        version: "Windows 11 教育版",
+        key: "NW6C2-QMPVW-D7KKK-3GKT6-VCFB2",
+      },
+      {
+        version: "Windows 10 教育版",
+        key: "NW6C2-QMPVW-D7KKK-3GKT6-VCFB2",
+      },
+      {
+        version: "Windows 11 教育版 N",
+        key: "2WH4N-8QGBV-H22JP-CT43Q-MDWWJ",
+      },
+      {
+        version: "Windows 10 教育版 N",
+        key: "2WH4N-8QGBV-H22JP-CT43Q-MDWWJ",
+      },
+      {
+        version: "Windows 11 企业版",
+        key: "NPPR9-FWDCX-D2C8J-H872K-2YT43",
+      },
+      {
+        version: "Windows 10 企业版",
+        key: "NPPR9-FWDCX-D2C8J-H872K-2YT43",
+      },
+      {
+        version: "Windows 11 企业版 N",
+        key: "DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4",
+      },
+      {
+        version: "Windows 10 企业版 N",
+        key: "DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4",
+      },
+      {
+        version: "Windows 11 企业版 G",
+        key: "YYVX9-NTFWV-6MDM3-9PT4T-4M68B",
+      },
+      {
+        version: "Windows 11 企业版 G",
+        key: "YYVX9-NTFWV-6MDM3-9PT4T-4M68B",
+      },
+      {
+        version: "Windows 11 企业版 G N",
+        key: "44RPN-FTY23-9VTTB-MP9BX-T84FV",
+      },
+      {
+        version: "Windows 10 企业版 G N",
+        key: "44RPN-FTY23-9VTTB-MP9BX-T84FV",
+      },
+    ],
+  },
+  WINDOWS_10_LTSC_2021_2019: {
+    key: "Windows 10 LTSC 2021 和 2019",
+    value: [
+      {
+        version: "Windows 10 企业版 LTSC 2021",
+        key: "M7XTQ-FN8P6-TTKYV-9D4CC-J462D",
+      },
+      {
+        version: "Windows 10 企业版 LTSC 2019",
+        key: "M7XTQ-FN8P6-TTKYV-9D4CC-J462D",
+      },
+      {
+        version: "Windows 10 企业版 N LTSC 2021",
+        key: "92NFX-8DJQP-P6BBQ-THF9C-7CG2H",
+      },
+      {
+        version: "Windows 10 企业版 N LTSC 2019",
+        key: "92NFX-8DJQP-P6BBQ-THF9C-7CG2H",
+      },
+    ],
+  },
+  WINDOWS_10_LTSB_2016: {
+    key: "Windows 10 LTSB 2016",
+    value: [
+      {
+        version: "Windows 10 企业版 LTSB 2016",
+        key: "DCPHK-NFMTC-H88MJ-PFHPY-QJ4BJ",
+      },
+      {
+        version: "Windows 10 企业版 N LTSB 2016",
+        key: "QFFDN-GRT3P-VKWWX-X7T3R-8B639",
+      },
+    ],
+  },
+  WINDOWS_10_LTSB_2015: {
+    key: "Windows 10 LTSB 2015",
+    value: [
+      {
+        version: "Windows 10 企业版 2015 LTSB",
+        key: "WNMTR-4C88C-JK8YV-HQ7T2-76DF9",
+      },
+      {
+        version: "Windows 10 企业版 2015 LTSB N",
+        key: "2F77B-TNFGY-69QQF-B8YKP-D69TJ",
+      },
+    ],
+  },
+  WINDOWS_SERVER_1803: {
+    key: "Windows Server 版本 1803",
+    value: [
+      {
+        version: "Windows Server Datacenter",
+        key: "2HXDN-KRXHB-GPYC7-YCKFJ-7FVDG",
+      },
+      {
+        version: "Windows Server Standard",
+        key: "PTXN8-JFHJM-4WC78-MPCBR-9W4KR",
+      },
+    ],
+  },
+  WINDOWS_SERVER_1709: {
+    key: "Windows Server 版本 1709",
+    value: [
+      {
+        version: "Windows Server Datacenter",
+        key: "6Y6KB-N82V8-D8CQV-23MJW-BWTG6",
+      },
+      {
+        version: "Windows Server Standard",
+        key: "DPCNP-XQFKJ-BJF7R-FRC8D-GF6G4",
+      },
+    ],
+  },
+  WINDOWS_SERVER_2012_R2: {
+    key: "Windows Server 2012 R2",
+    value: [
+      {
+        version: "Windows Server 2012 R2 Standard",
+        key: "D2N9P-3P6X9-2R39C-7RTCD-MDVJX",
+      },
+      {
+        version: "Windows Server 2012 R2 Datacenter",
+        key: "W3GGN-FT8W3-Y4M27-J84CP-Q3VJ9",
+      },
+      {
+        version: "Windows Server 2012 R2 Essentials",
+        key: "KNC87-3J2TX-XB4WP-VCPJV-M4FWM",
+      },
+    ],
+  },
+  WINDOWS_SERVER_2012: {
+    key: "Windows Server 2012",
+    value: [
+      {
+        version: "Windows Server 2012",
+        key: "BN3D2-R7TKB-3YPBD-8DRP2-27GG4",
+      },
+      {
+        version: "Windows Server 2012 N",
+        key: "8N2M2-HWPGY-7PGT9-HGDD8-GVGGY",
+      },
+      {
+        version: "Windows Server 2012 单语言版",
+        key: "2WN2H-YGCQR-KFX6K-CD6TF-84YXQ",
+      },
+      {
+        version: "Windows Server 2012 特定国家/地区版",
+        key: "4K36P-JN4VD-GDC6V-KDT89-DYFKP",
+      },
+      {
+        version: "Windows Server 2012 Standard",
+        key: "XC9B7-NBPP2-83J2H-RHMBY-92BT4",
+      },
+      {
+        version: "Windows Server 2012 MultiPoint 标准版",
+        key: "HM7DN-YVMH3-46JC3-XYTG7-CYQJJ",
+      },
+      {
+        version: "Windows Server 2012 MultiPoint 高级版",
+        key: "XNH6W-2V9GX-RGJ4K-Y8X6F-QGJ2G",
+      },
+      {
+        version: "Windows Server 2012 Datacenter",
+        key: "48HP8-DN98B-MYWDG-T2DCC-8W83P",
+      },
+    ],
+  },
+  WINDOWS_SERVER_2008_R2: {
+    key: "Windows Server 2008 R2",
+    value: [
+      {
+        version: "Windows Server 2008 R2 Web 版",
+        key: "6TPJF-RBVHG-WBW2R-86QPH-6RTM4",
+      },
+      {
+        version: "Windows Server 2008 R2 HPC 版",
+        key: "TT8MH-CG224-D3D7Q-498W2-9QCTX",
+      },
+      {
+        version: "Windows Server 2008 R2 标准版",
+        key: "YC6KT-GKW9T-YTKYR-T4X34-R7VHC",
+      },
+      {
+        version: "Windows Server 2008 R2 企业版",
+        key: "489J6-VHDMP-X63PK-3K798-CPX3Y",
+      },
+      {
+        version: "Windows Server 2008 R2 Datacenter",
+        key: "74YFP-3QFB3-KQT8W-PMXWJ-7M648",
+      },
+      {
+        version: "面向基于 Itanium 系统的 Windows Server 2008 R2",
+        key: "GT63C-RJFQ3-4GMB6-BRFB9-CB83V",
+      },
+    ],
+  },
+  WINDOWS_SERVER_2008: {
+    key: "Windows 2008 Server",
+    value: [
+      {
+        version: "Windows Web Server 2008",
+        key: "WYR28-R7TFJ-3X2YQ-YCY4H-M249D",
+      },
+      {
+        version: "Windows Server 2008 标准版",
+        key: "TM24T-X9RMF-VWXK6-X8JC9-BFGM2",
+      },
+      {
+        version: "不带 Hyper-V 的 Windows Server 2008 标准版",
+        key: "W7VD6-7JFBR-RX26B-YKQ3Y-6FFFJ",
+      },
+      {
+        version: "Windows Server 2008 企业版",
+        key: "YQGMW-MPWTJ-34KDK-48M3W-X4Q6V",
+      },
+      {
+        version: "不带 Hyper-V 的 Windows Server 2008 企业版",
+        key: "39BXF-X8Q23-P2WWT-38T2F-G3FPG",
+      },
+      {
+        version: "Windows Server 2008 HPC",
+        key: "RCTX3-KWVHP-BR6TB-RB6DM-6X7HP",
+      },
+      {
+        version: "Windows Server 2008 Datacenter",
+        key: "7M67G-PC374-GR742-YH8V4-TCBY3",
+      },
+      {
+        version: "不带 Hyper-V 的 Windows Server 2008 数据中心版",
+        key: "22XQ2-VRXRG-P8D42-K34TD-G3QQC",
+      },
+      {
+        version: "面向基于 Itanium 系统的 Windows Server 2008",
+        key: "4DWFP-JF3DJ-B7DTH-78FJB-PDRHK",
+      },
+    ],
+  },
+  "WINDOWS_8.1": {
+    key: "Windows 8.1",
+    value: [
+      {
+        version: "Windows 8.1 专业版",
+        key: "GCRJD-8NW9H-F2CDX-CCM8D-9D6T9",
+      },
+      {
+        version: "Windows 8.1 专业版 N",
+        key: "HMCNV-VVBFX-7HMBH-CTY9B-B4FXY",
+      },
+      {
+        version: "Windows 8.1 企业版",
+        key: "MHF9N-XY6XB-WVXMC-BTDCT-MKKG7",
+      },
+      {
+        version: "Windows 8.1 企业版 N",
+        key: "TT4HM-HN7YT-62K67-RGRQJ-JFFXW",
+      },
+    ],
+  },
+  WINDOWS_8: {
+    key: "Windows 8",
+    value: [
+      {
+        version: "Windows 8 专业版",
+        key: "NG4HW-VH26C-733KW-K6F98-J8CK4",
+      },
+      {
+        version: "Windows 8 专业版 N",
+        key: "XCVCF-2NXM9-723PB-MHCB7-2RYQQ",
+      },
+      {
+        version: "Windows 8 企业版",
+        key: "32JNW-9KQ84-P47T8-D8GGY-CWCK7",
+      },
+      {
+        version: "Windows 8 企业版 N",
+        key: "JMNMF-RHW7P-DMY6X-RF3DR-X2BQT",
+      },
+    ],
+  },
+  WINDOWS_7: {
+    key: "Windows 7",
+    value: [
+      {
+        version: "Windows 7 专业版",
+        key: "FJ82H-XT6CR-J8D7P-XQJJ2-GPDD4",
+      },
+      {
+        version: "Windows 7 专业版 N",
+        key: "MRPKT-YTG23-K7D7T-X2JMM-QY7MG",
+      },
+      {
+        version: "Windows 7 专业版 E",
+        key: "W82YF-2Q76Y-63HXB-FGJG9-GF7QX",
+      },
+      {
+        version: "Windows 7 企业版",
+        key: "33PXH-7Y6KF-2VJC9-XBBR8-HVTHH",
+      },
+      {
+        version: "Windows 7 企业版 N",
+        key: "YDRBP-3D83W-TY26F-D46B2-XCKRJ",
+      },
+      {
+        version: "Windows 7 企业版 E",
+        key: "C29WB-22CC8-VJ326-GHFJW-H9DH4",
+      },
+    ],
+  },
+  WINDOWS_VISTA: {
+    key: "Windows Vista",
+    value: [
+      {
+        version: "Windows Vista 商用版",
+        key: "YFKBB-PQJJV-G996G-VWGXY-2V3X8",
+      },
+      {
+        version: "Windows Vista 商用版 N",
+        key: "HMBQG-8H2RH-C77VX-27R82-VMQBT",
+      },
+      {
+        version: "Windows Vista 企业版",
+        key: "VKK3X-68KWM-X2YGT-QR4M6-4BWMV",
+      },
+      {
+        version: "Windows Vista 企业版 N",
+        key: "VTC42-BM838-43QHV-84HX6-XJXKV",
+      },
+    ],
+  },
+};
---
+++ src/components/GVLKTable/index.tsx
@@ -0,0 +1,72 @@
+/**
+ * @description GVLK 表格组件
+ */
+
+import React from "react";
+import { Table, TableColumnsType } from "antd";
+
+import CopyButton from "../CopyButton";
+import dataSources from "./dataSources";
+
+interface Props {
+  type:
+    | "WINDOWS_SERVER_2022"
+    | "WINDOWS_SERVER_2019"
+    | "WINDOWS_SERVER_2016"
+    | "WINDOWS_SERVER_20H2_2004_1909_1809"
+    | "WINDOWS_11_10"
+    | "WINDOWS_10_LTSC_2021_2019"
+    | "WINDOWS_10_LTSB_2016"
+    | "WINDOWS_10_LTSB_2015"
+    | "WINDOWS_SERVER_1803"
+    | "WINDOWS_SERVER_1709"
+    | "WINDOWS_SERVER_2012_R2"
+    | "WINDOWS_SERVER_2012"
+    | "WINDOWS_SERVER_2008_R2"
+    | "WINDOWS_SERVER_2008"
+    | "WINDOWS_8.1"
+    | "WINDOWS_8"
+    | "WINDOWS_7"
+    | "WINDOWS_VISTA";
+}
+
+const GVLKTable: React.FC<Props> = (props) => {
+  const { type } = props;
+
+  const columns: TableColumnsType = [
+    {
+      title: "操作系统版本 ",
+      dataIndex: "version",
+      width: 330,
+      render: (_: string) => _,
+    },
+    {
+      title: "KMS 客户端产品密钥 ",
+      dataIndex: "key",
+      width: 300,
+      render: (_: string) => _,
+    },
+    {
+      title: "操作",
+      dataIndex: "operation",
+      width: 110,
+      render: (_: string, record: any) => {
+        return <CopyButton text={record.key} />;
+      },
+    },
+  ];
+
+  console.log("type", type, dataSources[type].value);
+
+  return (
+    <Table
+      size="middle"
+      pagination={false}
+      columns={columns}
+      scroll={{ x: 734 }}
+      dataSource={dataSources[type].value}
+    />
+  );
+};
+
+export default GVLKTable;
